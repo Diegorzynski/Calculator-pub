88670266707dea18ac7c27d9b8ae84f2803b7828
diegorzynski <diegorzynski@hotmail.com>
2021-06-08 13:24:44 +0200

    added operations

---
+++ index.js
@@ -0,0 +1,26 @@
+let num1 = 8
+let num2 = 2
+document.getElementById("num1-el").textContent = num1 
+document.getElementById("num2-el").textContent = num2 
+let sumEl = document.getElementById("sum-el");
+
+
+function add(){
+    result = num1 + num2 
+    sumEl.textContent = "Result " + result
+}
+
+function subtract() {
+    result = num1 - num2 
+    sumEl.textContent = "Result " + result
+}
+
+function devide(){
+    result = num1 / num2 
+    sumEl.textContent = "Result " + result
+}
+
+function multiply(){
+    result = num1 * num2 
+    sumEl.textContent = "Result " + result
+};
